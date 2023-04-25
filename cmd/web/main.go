765f89e9b6f52b819e1a2ce83bc644f71e2cd5f5
Dragan Mijatovic <gandra404@yahoo.com>
2023-04-25 23:10:50 +0200

    refactor code to use packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/gandra/helloworld-web-go-trevor-sawler/pkg/handlers"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println("Starting application on port", portNumber)
 	http.ListenAndServe(portNumber, nil)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,4 +1,4 @@
-package main
+package render
 
 import (
 	"fmt"
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-// RenderTemplate is func to render template
+// RenderTemplate rendersemplates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
